01515ba0009636551ba40830ba89732a15e2f883
Lotyself <ahmetcakirforwork@gmail.com>
2022-12-30 21:58:49 +0300

    I have lasagna in my ass
    please send help

--- js/hamburger.js
+++ js/hamburger.js
@@ -3,11 +3,17 @@ window.onload = function(){
     const menu_btn = document.querySelector(".hamburger");
     const mobile_menu = document.querySelector(".sliding-menu");
     const black = document.querySelector(".blackblackblackeverythingisblack");
+    const aboutbutton = document.querySelector(".about");
+    const aboutmenu = document.querySelector(".sliding-about-section");
 
     menu_btn.addEventListener("click", function(){
         menu_btn.classList.toggle("is-active");
         mobile_menu.classList.toggle("is-active");
         black.classList.toggle("is-active");
-    })
 
+    })
+    aboutbutton.addEventListener("click", function(){
+        aboutmenu.classList.toggle("is-active");
+        aboutbutton.classList.toggle("is-active")
+    })
 }

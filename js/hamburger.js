9b71e306fc5a99f9fafa97c8ebd6a68d7f3aa302
Lotyself <ahmetcakirforwork@gmail.com>
2023-01-07 21:33:51 +0300

    asd

--- js/hamburger.js
+++ js/hamburger.js
@@ -6,7 +6,9 @@ window.onload = function(){
     const aboutbutton = document.querySelector(".about");
     const aboutmenu = document.querySelector(".sliding-about-section");
     const faqbutton = document.querySelector(".faq");
-    const faqmenu = document.querySelector(".sliding-faq-section")
+    const faqmenu = document.querySelector(".sliding-faq-section");
+    const mapbutton = document.querySelector(".map");
+    const mapmenu = document.querySelector(".sliding-map-section");
 
     menu_btn.addEventListener("click", function(){
         menu_btn.classList.toggle("is-active");
@@ -17,6 +19,8 @@ window.onload = function(){
         aboutbutton.classList.remove("is-active");
         faqbutton.classList.remove("is-active");
         faqmenu.classList.remove("is-active");
+        mapbutton.classList.remove("is-active");
+        mapmenu.classList.remove("is-active");
         
 
     })
@@ -26,6 +30,8 @@ window.onload = function(){
 
         faqbutton.classList.remove("is-active");
         faqmenu.classList.remove("is-active");
+        mapbutton.classList.remove("is-active");
+        mapmenu.classList.remove("is-active");
     })
 
     faqbutton.addEventListener("click", function(){
@@ -34,4 +40,18 @@ window.onload = function(){
 
         aboutbutton.classList.remove("is-active");
         aboutmenu.classList.remove("is-active");
+        mapbutton.classList.remove("is-active");
+        mapmenu.classList.remove("is-active");
+    })
+
+    mapbutton.addEventListener("click", function(){
+        mapbutton.classList.toggle("is-active");
+        mapmenu.classList.toggle("is-active");
+
+        aboutbutton.classList.remove("is-active");
+        aboutmenu.classList.remove("is-active");
+        faqbutton.classList.remove("is-active");
+        faqmenu.classList.remove("is-active");
     })}
+    
+    
